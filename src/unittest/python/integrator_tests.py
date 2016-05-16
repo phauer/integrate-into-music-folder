badbd8e07d103b2e5dc620e047e0f6d9e6bffa55
phauer <info@philipphauer.de>
2016-05-16 17:42:31 +0200

    refactor integrator tests + added another test

--- src/unittest/python/integrator_tests.py
+++ src/unittest/python/integrator_tests.py
@@ -1,4 +1,3 @@
-import sys
 import unittest
 
 from path import path  # path.py
@@ -15,26 +14,24 @@ class TestStringMethods(unittest.TestCase):
     def setUp(self):
         TEST_OUTPUT.rmtree()
         TEST_OUTPUT.makedirs()
+        test_name = self.id().split(".")[-1]
+        self.root_path = TEST_OUTPUT.joinpath(test_name)
+        self.root_path.makedirs()
+        print("====== {} ====== ".format(test_name))
 
     def test_happy_path(self):
-        test_name = sys._getframe().f_code.co_name
-        root_path = TEST_OUTPUT.joinpath(test_name)
-        clear(root_path)
         downloads_source_tree = Tree()
-        downloads_source_tree.create_node(identifier="download_folder_1")
-        downloads_source_tree.create_node(identifier="Should Not Be Used", parent="download_folder_1")
-        downloads_source_tree.create_node(identifier="F-S-F---W-I-B--T-I-H--2013", parent="download_folder_1")
+        downloads_source_tree.create_node(identifier="download_folder")
+        downloads_source_tree.create_node(identifier="Should Not Be Used", parent="download_folder")
+        downloads_source_tree.create_node(identifier="F-S-F---W-I-B--T-I-H--2013", parent="download_folder")
         downloads_source_tree.create_node(identifier="F S F - W I B, T I H (2013)", parent="F-S-F---W-I-B--T-I-H--2013")
         downloads_source_tree.create_node(identifier="song1.mp3", parent="F S F - W I B, T I H (2013)")
         downloads_source_tree.create_node(identifier="song2.mp3", parent="F S F - W I B, T I H (2013)")
         downloads_source_tree.create_node(identifier="song3.mp3", parent="F S F - W I B, T I H (2013)")
-        print("Creating test downloads folder:")
-        downloads_source_tree.show()
-        created_download_root_folder = create_dummy_download_folder(root_path, downloads_source_tree)
+        created_download_root_folder = create_dummy_download_folder_with_output(self.root_path, downloads_source_tree)
 
-        music_folder = root_path.joinpath("music_folder_1")
-        if not music_folder.exists():
-            music_folder.mkdir()
+        music_folder = self.root_path.joinpath("music_folder")
+        music_folder.mkdir()
 
         integrator.integrate(
             source_download_folder=created_download_root_folder,
@@ -42,12 +39,31 @@ class TestStringMethods(unittest.TestCase):
             simulate=False)
 
         expected_music_folder_tree = Tree()
-        expected_music_folder_tree.create_node(identifier="music_folder_1")
-        expected_music_folder_tree.create_node(identifier="F S F", parent="music_folder_1")
+        expected_music_folder_tree.create_node(identifier="music_folder")
+        expected_music_folder_tree.create_node(identifier="F S F", parent="music_folder")
         expected_music_folder_tree.create_node(identifier="W I B, T I H (2013)", parent="F S F")
         expected_music_folder_tree.create_node(identifier="song1.mp3", parent="W I B, T I H (2013)")
         expected_music_folder_tree.create_node(identifier="song2.mp3", parent="W I B, T I H (2013)")
         expected_music_folder_tree.create_node(identifier="song3.mp3", parent="W I B, T I H (2013)")
+        self.compare_actual_folder_with_tree_with_output(self.root_path, expected_music_folder_tree)
+
+    def test_latest_folder_contains_nothing(self):
+        downloads_source_tree = Tree()
+        downloads_source_tree.create_node(identifier="download_folder")
+        downloads_source_tree.create_node(identifier="Should Not Be Used", parent="download_folder")
+        downloads_source_tree.create_node(identifier="F-S-F---W-I-B--T-I-H--2013", parent="download_folder")
+        created_download_root_folder = create_dummy_download_folder_with_output(self.root_path, downloads_source_tree)
+
+        music_folder = self.root_path.joinpath("music_folder")
+        music_folder.mkdir()
+
+        with self.assertRaisesRegex(integrator.IntegrationError, "'The latest folder .* has no child folder.'"):
+            integrator.integrate(
+                source_download_folder=created_download_root_folder,
+                target_music_folder=music_folder,
+                simulate=False)
+
+    def compare_actual_folder_with_tree_with_output(self, root_path, expected_music_folder_tree):
         print("Expecting the following music folder:")
         expected_music_folder_tree.show()
         self.compare_actual_folder_with_tree(root_path, expected_music_folder_tree)
@@ -61,6 +77,13 @@ class TestStringMethods(unittest.TestCase):
         for children in children:
             subtree = tree.subtree(children.identifier)
             self.compare_actual_folder_with_tree(root_path, subtree)
+
+
+def create_dummy_download_folder_with_output(root_path, downloads_source_tree):
+    print("Creating test downloads folder:")
+    downloads_source_tree.show()
+    created_download_root_folder = create_dummy_download_folder(root_path, downloads_source_tree)
+    return created_download_root_folder
 
 
 def create_dummy_download_folder(root, tree):
